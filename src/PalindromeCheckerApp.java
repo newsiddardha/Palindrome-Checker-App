7649d6dd379194f2a0b78f236c5b6b5a21c28f9d
newsiddardha <mohanmaddimsetti@gmail.com>
2026-03-11 00:29:32 +0530

    UC13:palindrome exection time

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,59 +1,20 @@
-import java.util.*;
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
-        }
-
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String input = "level";
-        PalindromeStrategy strategy;
-        strategy = new StackStrategy();
 
-        boolean result = strategy.check(input);
-
-        if (result) {
-            System.out.println(input + " is a Palindrome");
-        } else {
-            System.out.println(input + " is not a Palindrome");
+        long startTime = System.nanoTime();
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
         }
+        boolean isPalindrome = input.equals(reversed);
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Input String: " + input);
+        System.out.println("Palindrome Result: " + isPalindrome);
+        System.out.println("Execution Time (Reverse Method): " + (endTime - startTime) + " ns");
     }
 }
