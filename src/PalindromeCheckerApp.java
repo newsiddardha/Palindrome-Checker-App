c74c953f95abaad05fbbba73e3ec5e5414564786
newsiddardha <mohanmaddimsetti@gmail.com>
2026-03-10 19:30:37 +0530

    UC3:palindrome of give string

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String word = "madam";
-        String reversed ="";
-        for(int i=word.length()-1;i>=0;i--){
-            reversed=reversed+word.charAt(i);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a string :");
+        String word = sc.nextLine();
+        String reversed = "";
+        for(int i = word.length()-1;i>=0;i--){
+            reversed = reversed + word.charAt(i);
         }
-        if (word.equals(reversed)){
-            System.out.println(word+ " is a Palindrome");
+        if(word.equals(reversed)){
+            System.out.print("the given string is a palindrome");
         }else{
-            System.out.println(word+ " is not a Palindrome");
+            System.out.print("the given string is not a palindrome");
         }
+        sc.close();
+
     }
 }
